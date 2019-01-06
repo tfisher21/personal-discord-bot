012a59e84ee51a1d72bb0b32896e4f6da04fb512
Tyler Fisher <tfisher21@gmail.com>
2019-01-06 16:36:15 -0600

    Add Twit node to allow streaming connection to twitter.

--- bot.js
+++ bot.js
@@ -1,10 +1,16 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const axios = require("axios");
+const Twit = require("twit");
 
 require("dotenv").config();
 
-const botToken = process.env.BOT_TOKEN;
+const T = new Twit({
+  consumer_key: process.env.CONSUMER_API_KEY,
+  consumer_secret: process.env.CONSUMER_SECRET_API_KEY,
+  access_token: process.env.ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.ACCESS_TOKEN_SECRET_KEY
+});
 
 client.once("ready", () => {
   console.log("Ready!");
@@ -20,23 +26,19 @@ client.on("message", message => {
       "Workers of the world unite; you have nothing to lose but your chains."
     );
   }
-
-  // If the message is "how to embed"
-  if (message.content === "how to embed") {
-    // We can create embeds using the MessageEmbed constructor
-    // Read more about all that you can do with the constructor
-    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
-    // const embed = new Discord.RichEmbed()
-    //   // Set the title of the field
-    //   .setTitle("A slick little embed")
-    //   // Set the color of the embed
-    //   .setColor(0xff0000)
-    //   // Set the main content of the embed
-    //   .setDescription("Hello, this is a slick embed!");
-    // // Send the embed to the same channel as the message
-    // message.channel.send(embed);
-    message.channel.send("https://twitter.com/AOC/status/1081566337225375745");
-  }
 });
 
-client.login(botToken);
+// @AOC Twitter Embed
+const AOC = "138203134";
+const tfisher21 = "14386881";
+const chicagobears = "47964412";
+const stream = T.stream("statuses/filter", {
+  follow: chicagobears,
+  filter_level: "medium"
+});
+
+stream.on("tweet", tweet => {
+  console.log(tweet);
+});
+
+client.login(process.env.BOT_TOKEN);
