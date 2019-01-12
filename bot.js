39401741475abedef6df26c62ea70774ae48c51c
Tyler Fisher <tfisher21@gmail.com>
2019-01-12 10:18:56 -0600

    Integrate the twitter component into the discord bot and make it more modular.

--- bot.js
+++ bot.js
@@ -12,14 +12,16 @@ const T = new Twit({
   access_token_secret: process.env.ACCESS_TOKEN_SECRET_KEY
 });
 
-let aocChannel = "";
+let twitterChannel = "";
+let twitterID = "";
+let twitterUser = "";
 
 client.once("ready", () => {
   console.log("Ready!");
 
-  aocChannel = client.channels.get("530851329720057857");
-  // console.log(aocChannel.id);
-  // aocChannel.send("Testing");
+  twitterChannel = client.channels.get("530851329720057857");
+  // console.log(twitterChannel.id);
+  // twitterChannel.send("Testing");
 });
 
 client.on("message", message => {
@@ -33,26 +35,60 @@ client.on("message", message => {
       "Workers of the world unite; you have nothing to lose but your chains."
     );
   }
+  const twitterPrefix = "!set twitter";
+  if (
+    message.channel === twitterChannel &&
+    message.content.startsWith(twitterPrefix)
+  ) {
+    twitterUser = message.content.slice(twitterPrefix.length + 1);
+    T.get("users/lookup", { screen_name: twitterUser }, function(
+      err,
+      data,
+      response
+    ) {
+      twitterID = data[0].id_str;
+      // console.log(data);
+      message.channel.send("Twitter User: " + twitterUser);
+      message.channel.send("Twitter ID: " + twitterID);
+      const stream = T.stream("statuses/filter", {
+        follow: twitterID
+        // filter_level: "low"
+      });
+
+      stream.on("tweet", tweet => {
+        if (tweet.user.id_str === twitterID) {
+          console.log(tweet);
+          twitterChannel
+            .send(
+              "https://twitter.com/" + twitterUser + "/status/" + tweet.id_str
+            )
+            .catch(errors => {
+              console.log(errors);
+            });
+        }
+      });
+    });
+  }
+  if (message.content === "!currenttwitter") {
+    message.channel.send("Currently Following " + twitterUser);
+  }
 });
 
 // @AOC Twitter Embed
-const AOC = "138203134";
-const tfisher21 = "14386881";
-const metaphorminute = "575930104";
-const stream = T.stream("statuses/filter", {
-  follow: AOC
-  // filter_level: "low"
-});
+// const stream = T.stream("statuses/filter", {
+//   follow: twitterID
+//   // filter_level: "low"
+// });
 
-stream.on("tweet", tweet => {
-  if (tweet.user.id_str === AOC) {
-    console.log(tweet);
-    aocChannel
-      .send("https://twitter.com/AOC/status/" + tweet.id_str)
-      .catch(errors => {
-        console.log(errors);
-      });
-  }
-});
+// stream.on("tweet", tweet => {
+//   if (tweet.user.id_str === twitterID) {
+//     console.log(tweet);
+//     twitterChannel
+//       .send("https://twitter.com/" + twitterUser + "/status/" + tweet.id_str)
+//       .catch(errors => {
+//         console.log(errors);
+//       });
+//   }
+// });
 
 client.login(process.env.BOT_TOKEN);
